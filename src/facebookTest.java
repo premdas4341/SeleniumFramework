9b058aed4020a33e31dc883623d11398d512bb3f
premdas4341 <37986766+premdas4341@users.noreply.github.com>
2018-10-20 21:06:21 -0400

    facebookcode

--- src/facebookTest.java
+++ src/facebookTest.java
@@ -1,9 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class facebookTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\premd\\\\eclipse-workspace\\\\Feb2018\\\\jars\\\\chromedriver_win32\\\\chromedriver.exe");
+		
+		//os
+		WebDriver driver=new ChromeDriver();
+		//Different browsers
+		driver.get("http://www.facebook.com");
+//		qa.facebook.com
+//		stage.facebookTest.com
 
+
+		String fname = "//input[@name='firstname']";
+		String lname = "//input[@name='lastname']";
+		
+		// reusable function
+		
+		driver.findElement(By.xpath(fname)).sendKeys("Hassan");
+		driver.findElement(By.xpath(lname)).sendKeys("Bhuiyan");
+		// less duplicate or less code
+		
 	}
 
 }
