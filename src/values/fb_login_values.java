9fbddbb29856fdd2c6d7c1966f470c9540c86725
premdas4341 <37986766+premdas4341@users.noreply.github.com>
2018-11-17 22:56:00 -0500

    feature1117

--- src/values/fb_login_values.java
+++ src/values/fb_login_values.java
@@ -5,5 +5,7 @@ public class fb_login_values {
 	public String emailPhone_value 	 		= "taltekc@gmail.com";
 	public String password_value 			= "Test.123";
 	public String password_invalid_value	= "asdfasdfasdfad";
+	
+	public String signUpValue 				= "Sign Up";
 		
 }
